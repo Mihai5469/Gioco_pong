f83cfd5137cdaf906a5ecda52a4ea8b7eec772e8
MIHAI IACHIMOVSCHI <MIHAI.IACHIMOVSCHI@ptpvenezia.edu.it>
2023-04-12 10:55:22 +0200

    start

--- Gioco_pong.cpp
+++ Gioco_pong.cpp
@@ -46,6 +46,7 @@ int main()
     int score = 0;
     int record = 0;
 
+    bool start = false;
 
     al_init();
     al_init_primitives_addon();
@@ -63,8 +64,9 @@ int main()
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();           //coda eventi
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / 244.0);            //timer
     ALLEGRO_FONT* font = al_load_ttf_font("YARDSALE.TTF", 30, 0);   //font
-    ALLEGRO_SAMPLE* suono = al_load_sample("ding2.mp3");                       //suono
-    ALLEGRO_SAMPLE* suono2 = al_load_sample("ding.mp3");                       //suono
+    ALLEGRO_FONT* font2 = al_load_ttf_font("YARDSALE.TTF", 10, 0);   //font
+    ALLEGRO_SAMPLE* suono = al_load_sample("ding2.mp3");            //suono
+    ALLEGRO_SAMPLE* suono2 = al_load_sample("ding.mp3");            //suono
 
     ALLEGRO_EVENT event;
 
@@ -122,6 +124,7 @@ int main()
 
         case ALLEGRO_EVENT_KEY_DOWN:
             key[event.keyboard.keycode] = KEY_SEEN | KEY_RELEASED;
+            start = true;
             break;
 
         case ALLEGRO_EVENT_KEY_UP:
@@ -136,93 +139,99 @@ int main()
         // controlla se si vuole chiedere il programma tramite tasto 'ESC' o la 'X' che chiede i programmi
         if (done)
             break;
+        //inizio del gioco
+        if (start) {
+            //movimento del rettangolo automatico
 
-        //movimento del rettangolo automatico
+            centroRet = (xrA1 + xrA2) / 2;
 
-        centroRet = (xrA1 + xrA2) / 2;
-
-        if (centroRet < xc && xrA2 < w - 2) {
-            xrA1++;
-            xrA2++;
-        }
-        else if (xrA1 > 2) {
-            xrA1--;
-            xrA2--;
-        }
-
-
-        //Rimbalzo laterale
-        //destra
-        if (xc < w - raggio && rimL == true) {
-            xc++;
-        }
-        else {
-            //suona
-            if (xc == w - raggio) {
-                al_play_sample(suono, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
-
+            if (centroRet < xc && xrA2 < w - 2) {
+                xrA1++;
+                xrA2++;
             }
-            rimL = false;
-        }
-
-        //sinistra
-        if (xc > 0 + raggio && rimL == false) {
-            xc--;
-        }
-        else {
-            //suona
-            if (xc == 0 + raggio) {
-                al_play_sample(suono, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
+            else if (xrA1 > 2) {
+                xrA1--;
+                xrA2--;
             }
-            rimL = true;
-        }
-        //Rimbalzo verticale
-        
-        //giu
-        if (yc < h - 20 - raggio && rimV == false ) {
-            yc++;
-        }
 
-        else if (xc >= xr1 && xc <= xr2) {
-            //suona
-            if (yc == h - 20 - raggio) {
-                al_play_sample(suono2, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
+
+            //Rimbalzo laterale
+            //destra
+            if (xc < w - raggio && rimL == true) {
+                xc++;
             }
-            rimV = true;
-        }
-        //su
-        if (yc > 0 + 20 + raggio && rimV == true) {
-            yc--;
-        }
-        else if (xc >= xrA1 && xc <= xrA2) {
-            //suona
-            if (yc == 0 + 20 + raggio) {
-                al_play_sample(suono2, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
+            else {
+                //suona
+                if (xc == w - raggio) {
+                    al_play_sample(suono, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
+
+                }
+                rimL = false;
             }
-            rimV = false;
-        }
+
+            //sinistra
+            if (xc > 0 + raggio && rimL == false) {
+                xc--;
+            }
+            else {
+                //suona
+                if (xc == 0 + raggio) {
+                    al_play_sample(suono, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
+                }
+                rimL = true;
+            }
+            //Rimbalzo verticale
+
+            //giu
+            if (yc < h - 20 - raggio && rimV == false) {
+                yc++;
+            }
+
+            else if (xc >= xr1 && xc <= xr2) {
+                //suona
+                if (yc == h - 20 - raggio) {
+                    al_play_sample(suono2, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
+                }
+                rimV = true;
+            }
+            //su
+            if (yc > 0 + 20 + raggio && rimV == true) {
+                yc--;
+            }
+            else if (xc >= xrA1 && xc <= xrA2) {
+                //suona
+                if (yc == 0 + 20 + raggio) {
+                    al_play_sample(suono2, 1.0, 0.0, 1.0, ALLEGRO_PLAYMODE_ONCE, NULL);
+                }
+                rimV = false;
+            }
 
 
 
-        // conteggio del puntegio
-        if (yc == h - 20 - raggio && xc >= xr1 && xc <= xr2)
-            score++;
+            // conteggio del puntegio
+            if (yc == h - 20 - raggio && xc >= xr1 && xc <= xr2)
+                score++;
 
-        // reseta tutto alle impostazioni iniziali trane il Record che viene agiornato
-        if (yc == h - 20 - raggio && (xc <= xr1 || xc >= xr2)) {
-            resetRetA(w, h, xrA1, xrA2, yrA1, yrA2);
-            resetRet(w, h, xr1, xr2, yr1, yr2);
-            resetPala(w, h, xc, yc, rimV, rimL);
-            resetScoreSaveRecord(score, record);   
+            // reseta tutto alle impostazioni iniziali trane il Record che viene agiornato
+            if (yc == h - 20 - raggio && (xc <= xr1 || xc >= xr2)) {
+                resetRetA(w, h, xrA1, xrA2, yrA1, yrA2);
+                resetRet(w, h, xr1, xr2, yr1, yr2);
+                resetPala(w, h, xc, yc, rimV, rimL);
+                resetScoreSaveRecord(score, record);
+                start = false;
+            }
         }
        
            
 
         al_clear_to_color(al_map_rgb(220, 220, 220));
 
-        al_draw_textf(font, al_map_rgb(50, 50, 50), w/2, h/2, ALLEGRO_ALIGN_CENTER, "Score: %d", score);
-        al_draw_textf(font, al_map_rgb(50, 50, 50), w / 2, h / 2 +40, ALLEGRO_ALIGN_CENTER, "Record: %d", record);
-
+        if (start) {
+            al_draw_textf(font2, al_map_rgb(50, 50, 50), 0, 20, 0, "Score: %d    Record: %d", score, record);
+        }
+        else {
+            al_draw_textf(font, al_map_rgb(50, 50, 50), w / 2, h / 2 + 50, ALLEGRO_ALIGN_CENTER, "Record: %d", record);
+        }
 
         al_draw_filled_rectangle(xrA1, yrA1, xrA2, yrA2, al_map_rgb(0, 0, 0));    //rettangolo computer
 
